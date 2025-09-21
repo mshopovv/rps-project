fd6b53e851cb838d1f9f2c5b71831202a6e33483
mshopovv <m.shopovv@gmail.com>
2025-09-21 18:45:58 +0300

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,4 +1,7 @@
 import random
+import colorama
+from colorama import Fore
+colorama.init(autoreset=True)
 
 rock = "Rock"
 paper = "Paper"
@@ -14,6 +17,7 @@ elif player_move == "s":
     player_move = scissors
 else:
     raise SystemExit("Invalid Input. Try again...")
+print(f"You chose {player_move}.")
 
 computer_random_number = random.randint(1, 3)
 computer_move = ""
@@ -30,9 +34,9 @@ print(f"The computer chose {computer_move}.")
 if (player_move == rock and computer_move == scissors) or \
         (player_move == paper and computer_move == rock) or \
         (player_move == scissors and computer_move == paper):
-    print("You win!")
+    print(Fore.GREEN + "You win!")
 elif player_move == computer_move:
-    print("Draw!")
+    print(Fore.YELLOW + "Draw!")
 else:
-    print("You lose!")
+    print(Fore.RED + "You lose!")
 
