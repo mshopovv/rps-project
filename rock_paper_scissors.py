a2bdebfd178719607ebfd02e5f695a96088449a0
mshopovv <m.shopovv@gmail.com>
2025-09-21 20:16:20 +0300

    pushed new cosmetic updates

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,21 +3,20 @@ import colorama
 from colorama import Fore
 colorama.init(autoreset=True)
 
-rock = "Rock"
-paper = "Paper"
-scissors = "Scissors"
+rock = "✊ - rock"
+paper = "✋ - paper"
+scissors = "✌️ - scissors"
 
 human_score = 0
 machine_score = 0
 print("Welcome to the Ultimate Rock, Paper, Scissors Showdown! ✊✋✌️\n"
       "Step into the arena where quick wits and sharp instincts rule. \n"
       "Will you crush with Rock, outsmart with Paper, or strike fast with Scissors?\n"
-      "Every choice counts — so think fast, play bold, and claim your victory!")
-print()
+      "Every choice counts — so think fast, play bold, and claim your victory!\n")
 print("👉 Ready? The game is simple. The thrill? Endless.")
-print()
+
 while True:
-    player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
+    player_move = input("\nChoose [r]ock, [p]aper or [s]cissors: ")
 
     if player_move == "r":
         player_move = rock
@@ -27,7 +26,7 @@ while True:
         player_move = scissors
     else:
         raise SystemExit("Invalid Input. Try again...")
-    print(f"You chose {player_move}.")
+    print(f"\nYou chose: {player_move}.")
 
     computer_random_number = random.randint(1, 3)
     computer_move = ""
@@ -39,7 +38,7 @@ while True:
     else:
         computer_move = scissors
 
-    print(f"The computer chose {computer_move}.")
+    print(f"The computer chose: {computer_move}.\n")
 
     if (player_move == rock and computer_move == scissors) or \
             (player_move == paper and computer_move == rock) or \
@@ -51,14 +50,17 @@ while True:
     else:
         print(Fore.RED + "You lose!")
         machine_score += 1
-
-    print(f"Current score:\nHuman: {human_score}\nMachine: {machine_score}")
-
+    print(f"\nCurrent score:\nHuman: {human_score}\nMachine: {machine_score}\n")
     continue_game = input("Do you want to continue playing? Type Yes or No: ")
+    print()
     if continue_game == "Yes" or continue_game == "yes" or continue_game == "y":
-        print()
         continue
     else:
-        print()
-        print(f"Thanks for playing! Final score:\nHuman: {human_score}\nMachine: {machine_score}")
+        print(f"\nThanks for playing! Final score:\nHuman: {human_score}\nMachine: {machine_score}\n")
+        if human_score > machine_score:
+            print("Victory for mankind!")
+        elif human_score == machine_score:
+            print("Better luck next time - it was a DRAW!")
+        else:
+            print("Victory for the machines!")
     break
